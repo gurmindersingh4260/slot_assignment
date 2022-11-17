9f264c8ece4a27f76ba5ccab09aaaf1465ebfe5c
gurmindersingh4260 <gurmindersingh4260@gmail.com>
2022-11-17 18:53:45 +0530

    fix:Added validations.

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -2,7 +2,7 @@
 
 class Slot < ApplicationRecord
   validates :start_time, presence: true
-  validates :end_time, presence: true
+  validates :end_time, presence: true , date: { after_or_equal_to: :start_date }
   validates :total_capacity, presence: true
 
   has_many :slot_collections
